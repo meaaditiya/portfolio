aabac5974201b6933e4ee4b83d858709d04bee03
Aaditiya Tyagi <aaditiyatyagi123@gmail.com>
2025-12-13 15:33:43 +0530

    security update

--- frontend/src/api/globalFetch.jsx
+++ frontend/src/api/globalFetch.jsx
@@ -9,7 +9,7 @@ const originalFetch = window.fetch;
 window.fetch = async (...args) => {
   const res = await originalFetch(...args);
 
-  if (res.status === 403 && nav) {
+  if (res.status === 403 || res.status ===429 && nav) {
     if (window.location.pathname !== "/forbidden") {
       nav("/forbidden");
     }
