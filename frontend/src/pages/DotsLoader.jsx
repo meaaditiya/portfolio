bc239ee4cf0c6831776b767eeb2c5dd9a82cc7dc
Aaditiya Tyagi <aaditiyatyagi123@gmail.com>
2025-12-15 23:05:28 +0530

    github Oauth

--- frontend/src/pages/DotsLoader.jsx
+++ frontend/src/pages/DotsLoader.jsx
@@ -3,21 +3,25 @@ import '../pagesCSS/DotsLoader.css';
 
 const CircleLoader = () => {
   return (
-    <div className="ultra_super_unique_loader_container_2025">
-      {[0, 1, 2, 3, 4, 5, 6, 7].map((i) => {
-        const angle = (i * 360) / 8;
-        return (
-          <div
-            key={i}
-            className="ultra_super_unique_loader_dot_2025"
-            style={{
-              transform: `rotate(${angle}deg) translate(30px) translateX(-50%)`,
-              animationDelay: `${i * 0.125}s`
-            }}
-          />
-        );
-      })}
+  <>
+  
+<section id="global">
+
+    <div id="top" class="mask">
+      <div class="plane"></div>
     </div>
+    <div id="middle" class="mask">
+      <div class="plane"></div>
+    </div>
+
+    <div id="bottom" class="mask">
+      <div class="plane"></div>
+    </div>
+    
+  <p className='loaderld'><i>LOADING...</i></p>
+    
+</section>
+  </>
   );
 };
 
