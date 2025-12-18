a2b160f0c00d1e15fe42ad1ad018b6cbbee08d7f
Aaditiya Tyagi <aaditiyatyagi123@gmail.com>
2025-12-19 00:50:20 +0530

    some UI/UX changes

--- frontend/src/pages/BlogPost.jsx
+++ frontend/src/pages/BlogPost.jsx
@@ -3069,7 +3069,7 @@ const CodeBlock = ({ language, value }) => {
           <button
             type="button"
             className="login-link-btn"
-            onClick={() => navigate('/userauth')}
+            onClick={() => navigate('/auth')}
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
               <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
